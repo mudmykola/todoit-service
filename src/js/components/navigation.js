9c337f08b8b583f321e0e4463843d29601499271
Mykola <46628437+mudmykola@users.noreply.github.com>
2023-02-16 07:31:54 +0200

    feat -> [style && function lang btn]

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,8 +1,22 @@
 const dropBtn = document.getElementsByClassName("navigation-dropdown__btn");
+const dropLang = document.getElementsByClassName("navigation-lang__dropdown--btn");
 let i;
 for (i = 0; i < dropBtn.length; i++) {
     dropBtn[i].addEventListener("click", function () {
         this.classList.toggle("active");
+        let cont = this.nextElementSibling;
+        if (cont.style.display === "flex") {
+            cont.style.display = "none";
+        } else {
+            cont.style.display = "flex";
+        }
+    });
+}
+;
+
+for (i = 0; i < dropLang.length; i++) {
+    dropLang[i].addEventListener("click", function () {
+        this.classList.toggle("show");
         let cont = this.nextElementSibling;
         if (cont.style.display === "flex") {
             cont.style.display = "none";
